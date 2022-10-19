04f36d2f70548582f0435e6e2dab4fe7b4fc747d
luckyman64 <clement.lantiat@gmail.com>
2022-10-19 09:51:27 +0200

    Initial Commit

---
+++ tmdb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tmdb
+//
+//  Created by Clement Lantiat on 27/07/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tmdb/tmdbApp.swift
@@ -0,0 +1,17 @@
+//
+//  tmdbApp.swift
+//  tmdb
+//
+//  Created by Clement Lantiat on 27/07/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct tmdbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

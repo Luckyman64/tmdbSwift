55431b5c394ec89193da2cd4058bc36ab6d424b3
luckyman64 <clement.lantiat@gmail.com>
2022-11-30 11:27:45 +0100

    enregistrement sur le tel

--- ViewModel/tmdbViewModel.swift
+++ ViewModel/tmdbViewModel.swift
@@ -10,7 +10,48 @@ import SwiftUI
 @MainActor
 class tmdbViewModel: ObservableObject{
     @Published var movies = [Movie]()
-    @Published var favoriteMovie : Set<Movie> = Set<Movie>()
+    @Published var favoriteMovie : Set<Movie> = Set<Movie>(){
+        didSet {
+            writeFavorite()
+        }
+    }
+    
+    init() {
+        readFavortite()
+    }
+    
+    func readFavortite(){
+        guard let documentURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        else{
+            return
+        }
+        
+        let favoriteURL = documentURL.appendingPathComponent("favorite.json")
+        
+        do{
+        let data = try Data(contentsOf: favoriteURL)
+            favoriteMovie = try JSONDecoder().decode(Set<Movie>.self, from: data)
+        }catch{
+            print(error)
+        }
+    }
+    
+    func writeFavorite(){
+        guard let documentURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        else{
+            return
+        }
+        
+        let favoriteURL = documentURL.appendingPathComponent("favorite.json")
+        
+        do{
+        if let data = try? JSONEncoder().encode(favoriteMovie){
+            try data.write(to: favoriteURL)
+        }
+        }catch{
+            print(error)
+        }
+    }
     
     let movieFetcher = MovieFetcher()
     
